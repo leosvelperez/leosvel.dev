0f8eeaf7e88c0df7a400d2860a50e51b2c9f14dd
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2021-12-28 16:00:44 +0000

    feat: add some animations

--- apps/website/tailwind.config.cjs
+++ apps/website/tailwind.config.cjs
@@ -5,6 +5,33 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        'slide-in-left': {
+          '0%': {
+            transform: 'translateX(-100px)',
+            opacity: 0.5,
+          },
+          '100%': {
+            transform: 'translateX(0)',
+            opacity: 1,
+          },
+        },
+        'zoom-in': {
+          '0%': {
+            opacity: 0.5,
+            transform: 'scale(0.7)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'scale(1)',
+          },
+        },
+      },
+      animation: {
+        'slide-in-left': 'slide-in-left 300ms ease-in-out',
+        'zoom-in': 'zoom-in 192ms ease-in-out',
+        'zoom-in-slow': 'zoom-in 300ms ease-in-out',
+      },
       listStyleType: {
         circle: 'circle',
         square: 'square',
