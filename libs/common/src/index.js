88561ab1574880c00f447b27e1778d4d60a7a901
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-01-23 16:35:27 +0000

    feat: add partytown and cloudflare web analytics

--- libs/common/src/index.js
+++ libs/common/src/index.js
@@ -1,4 +1,3 @@
 export { default as Footer } from './lib/components/Footer.astro';
-export { default as HeadCommon } from './lib/components/HeadCommon.astro';
+export { default as Head } from './lib/components/Head.astro';
 export { default as Header } from './lib/components/Header.astro';
-export { default as HeadSEO } from './lib/components/HeadSEO.astro';
