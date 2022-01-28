4cbfbb89c1f67af7671919d9bce25f8b410738c3
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-01-28 14:55:02 +0000

    feat: publish angular, tailwind, nx blog post

--- libs/blog/src/index.js
+++ libs/blog/src/index.js
@@ -1,3 +1,4 @@
 export { default as Blog } from './lib/Blog.astro';
 export { default as BlogPost } from './lib/BlogPost.astro';
 export { default as BlogPostPreview } from './lib/BlogPostPreview.astro';
+export { default as BlogPostAttribution } from './lib/BlogPostAttribution.astro';
